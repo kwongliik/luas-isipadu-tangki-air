00a66254f245e5d6f0f7827d0231cf203d297b3f
kwongliik <46083661+kwongliik@users.noreply.github.com>
2024-09-03 21:56:12 +0800

    test_luas.py dan test_isipadu.py

--- test_luas.py
+++ test_luas.py
@@ -29,18 +29,18 @@ def test_luas(monkeypatch, capsys):
     captured = capsys.readouterr()
     assert captured.out.strip() == "Luas tangki air = 131.96"
 
-def test_isipadu(monkeypatch, capsys):
-    # Define a function to simulate multiple user inputs
-    user_inputs = ["3","4"]
+# def test_isipadu(monkeypatch, capsys):
+#     # Define a function to simulate multiple user inputs
+#     user_inputs = ["3","4"]
 
-    def mock_input(_):
-        return user_inputs.pop(0)
+#     def mock_input(_):
+#         return user_inputs.pop(0)
 
-    # Use the function to simulate user input
-    monkeypatch.setattr('builtins.input', mock_input)
+#     # Use the function to simulate user input
+#     monkeypatch.setattr('builtins.input', mock_input)
 
-    luas_isipadu_tangki_air.isipadu()
+#     luas_isipadu_tangki_air.isipadu()
 
-    # Capture the printed output
-    captured = capsys.readouterr()
-    assert captured.out.strip() == "Isipadu tangki air = 113.11"
+#     # Capture the printed output
+#     captured = capsys.readouterr()
+#     assert captured.out.strip() == "Isipadu tangki air = 113.11"
